e5f953a6d2729cdc1acf908bafae8c6617d4cf98
Dylan Jones <dylanjones966@gmail.com>
2024-03-14 10:52:54 +0000

    Component test comits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { TestObj } from './models/TestObject';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sp_project_frontend';
+  title = 'sp_project_guide';
+  readonly test_url;
+
+  res = "No res";
+
+
+  constructor(private http: HttpClient){
+
+    this.test_url = "https://localhost:7171";
+
+    
+  }
+
+
+  simulateRequest(){
+  this.getReq().subscribe(r =>{
+      console.log(r);
+      
+    })
+  }
+
+  getReq(){
+    return this.http.get<TestObj>(`${this.test_url}/GetTemp`).pipe(map(r=>{
+      return {
+        id: r.id,
+        name: r.name,
+        description: r.description
+      }
+    }));
+
+  }
 }
